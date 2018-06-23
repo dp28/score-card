f1768cb1017dcd67cea853cdca4d237dce464a7a
Daniel Patterson <dan24patt@gmail.com>
2018-06-23 16:02:48 +0100

    Start intial react app with redux & material design

---
+++ score-card-web-ui/src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+export const App = () => (
+  <Button variant="contained" color="primary">
+    Start game
+  </Button>
+)
