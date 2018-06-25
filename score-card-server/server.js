38fc6bd9e906eb54309bb6df218850bf6b1606ac
Daniel Patterson <dan24patt@gmail.com>
2018-06-25 23:31:52 +0100

    Add basic websocket server
    To broadcast events between multiple clients interested in the same
    game. This is just a proof of concept, so is pretty rough.

---
+++ score-card-server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+require('express-ws')(app);
+
+const ServerPort = 8080;
+
+const clients = {};
+const games = {};
+
+function fetchGameForClient(gameId, clientId) {
+  if (!games.hasOwnProperty(gameId)) {
+    games[gameId] = {
+      clientIds: [clientId]
+    };
+    console.log(`New game (id: ${gameId}) - now ${Object.keys(games).length}`);
+  }
+  
+  const game = games[gameId];
+
+  if (!game.clientIds.includes(clientId)) {
+    game.clientIds.push(clientId);
+  }
+  return game;
+}
+
+app.get('/', (request, response) => {
+  response.send('Server is up and running ... try connecting over WebSockets');
+});
+
+app.ws('/', websocketConnection => {
+  const id = String(Math.random());
+  const client = { id, connection: websocketConnection };
+  clients[id] = client;
+
+  websocketConnection.on('message', rawMessage => {
+    const jsonMessage = JSON.parse(rawMessage);
+    const game = fetchGameForClient(jsonMessage.gameId, id);
+
+    game.clientIds.forEach(clientId => {
+      if (clientId !== id && clients.hasOwnProperty(clientId)) {
+        clients[clientId].connection.send(rawMessage);
+      }
+    })
+    websocketConnection.send(`broadcasted to ${game.clientIds.length - 1} other clients`);
+  });
+
+  websocketConnection.on('close', () => {
+    delete clients[id]
+    console.log(`Client disconnected - now ${Object.keys(clients).length} connected`);
+  })
+});
+
+app.listen(ServerPort);
+console.log(`Running at localhost:${ServerPort}`);
