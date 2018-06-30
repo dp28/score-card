bb41808e24ccd3bb2835a3553f1b8e64b9da7784
Daniel Patterson <dan24patt@gmail.com>
2018-07-01 00:39:23 +0100

    Add tests for join intent
    This was initially spiked without automated tests to ensure it works
    to integrate with Alexa. Now that it does, it needs tests.

--- score-card-server/src/alexa/intents/joinGameIntent.mjs
+++ score-card-server/src/alexa/intents/joinGameIntent.mjs
@@ -1,9 +1,8 @@
-import { selectTotals } from '../../score-card-domain.mjs'
 import { ID_ADJECTIVES, ID_ANIMALS } from '../customSlots.mjs'
 
 const ID_FORMAT = '{-|ADJECTIVE} {-|ANIMAL} {-|NUMBER}'
 
-export function buildJoinGameIntent({ gameManager }) {
+export function buildJoinGameIntent({ gameManager, domain }) {
   return {
     name: 'JoinGameIntent',
     schema: {
@@ -21,7 +20,7 @@ export function buildJoinGameIntent({ gameManager }) {
     requestHandler: (request, response) => {
       const idDetection = findIdInput(request)
       if (idDetection.valid) {
-        findGame({ idDetection, response, gameManager })
+        findGame({ idDetection, response, gameManager, domain })
       }
       else {
         response.say(`Sorry, that didn't seem to be a valid game id`)
@@ -43,18 +42,18 @@ function findIdInput(request) {
   }
 }
 
-function findGame({ response, gameManager, idDetection }) {
+function findGame({ response, gameManager, idDetection, domain }) {
   const game = gameManager.getCurrentGameState(idDetection.value)
   if (game) {
     response.say(`The current total scores are`)
-    response.say(buildCurrentScoresSentence(game))
+    response.say(buildCurrentScoresSentence(game, domain))
   } else {
     response.say(`I can't find a game with the id "${idDetection.input}"`)
   }
 }
 
-function buildCurrentScoresSentence(game,) {
-  const totals = selectTotals(game)
+function buildCurrentScoresSentence(game, domain) {
+  const totals = domain.selectTotals(game)
   const statements = totals.map(({ playerName, total }) => (
     `${playerName} has ${total} points`
   ))
@@ -62,12 +61,14 @@ function buildCurrentScoresSentence(game,) {
   switch (statements.length) {
     case 0: return 'unavailable as their are no players in the game.'
     case 1: return statements[0]
-    default: {
-      const lastTwoStatements = statements.slice(statements.length - 2)
-      const firstStatements = statements.slice(0, statements.length - 2)
-      const start = firstStatements.map(statement => `${statement}, `).join('')
-      const end = lastTwoStatements.join(' and ')
-      return start + end
-    }
+    default: return buildReadableLongList(statements)
   }
+}
+
+function buildReadableLongList(statements) {
+  const lastTwoStatements = statements.slice(statements.length - 2)
+  const firstStatements = statements.slice(0, statements.length - 2)
+  const start = firstStatements.map(statement => `${statement}, `).join('')
+  const end = lastTwoStatements.join(' and ')
+  return start + end
 }
---
+++ score-card-server/src/alexa/intents/joinGameIntent.spec.mjs
@@ -0,0 +1,123 @@
+import { buildJoinGameIntent } from './joinGameIntent.mjs'
+
+describe('JoinGameIntent', () => {
+  let intent
+  let gameManager
+  let selectTotals
+
+  beforeEach(() => {
+    selectTotals = jest.fn()
+    gameManager = {
+      getCurrentGameState: jest.fn()
+    }
+    intent = buildJoinGameIntent({ gameManager, domain: { selectTotals } })
+  })
+
+  describe('#requestHandler', () => {
+    let requestHandler
+    let request
+    let response
+    let call = () => requestHandler(request, response)
+
+    function getResponseText() {
+      return response.say.mock.calls.map(args => args[0]).join(' ')
+    }
+
+    beforeEach(() => {
+      request = {
+        slot: jest.fn()
+      }
+      response = {
+        say: jest.fn(),
+        shouldEndSession: jest.fn()
+      }
+      requestHandler = intent.requestHandler
+    })
+
+    it('should keep the session running', () => {
+      call()
+      expect(response.shouldEndSession.mock.calls).toEqual([[false]])
+    })
+
+    describe('when no id can be build from the input', () => {
+      beforeEach(() => {
+        request.slot = () => undefined
+      })
+
+      it('should tell the user that the id is wrong', () => {
+        call()
+        expect(getResponseText()).toMatch(/game\W+id/i)
+      })
+
+      it('should not try and fetch a game', () => {
+        call()
+        expect(gameManager.getCurrentGameState.mock.calls.length).toBe(0)
+      })
+    })
+
+    describe('when an id can be build from the input', () => {
+      beforeEach(() => {
+        request.slot = name => name.toLowerCase()
+      })
+
+      it('should try and fetch a game using the correct id format', () => {
+        call()
+        expect(gameManager.getCurrentGameState.mock.calls).toEqual([[
+          'adjective-animal-number'
+        ]])
+      })
+
+      describe('when no game can be found', () => {
+        it('should repeat the id to the user', () => {
+          call()
+          expect(getResponseText()).toMatch(/adjective animal number/)
+        })
+      })
+
+      describe('when the game can be found', () => {
+        beforeEach(() => gameManager.getCurrentGameState.mockReturnValue({}))
+
+        describe('without any players', () => {
+          it('should tell the user no players are in the game', () => {
+            selectTotals.mockReturnValue([])
+            call()
+            expect(getResponseText()).toMatch(/no players/)
+          })
+        })
+
+        describe('with one player', () => {
+          it('should tell the user the player\'s total', () => {
+            selectTotals.mockReturnValue([{ playerName: 'Daniel', total: 21 }])
+            call()
+            expect(getResponseText()).toMatch(/Daniel has 21 points/)
+          })
+        })
+
+        describe('with two players', () => {
+          it('should tell the user the players\' totals, separated by "and"', () => {
+            selectTotals.mockReturnValue([
+              { playerName: 'Daniel', total: 21 },
+              { playerName: 'John', total: 22 },
+            ])
+            call()
+            expect(getResponseText()).toMatch(/Daniel has 21 points and John has 22 points/)
+          })
+        })
+
+        describe('with three or more players', () => {
+          it('should tell the user the players\' totals, comma separated but "and" for the last two', () => {
+            selectTotals.mockReturnValue([
+              { playerName: 'Daniel', total: 21 },
+              { playerName: 'John', total: 22 },
+              { playerName: 'Joe', total: 32 }
+            ])
+            call()
+            expect(getResponseText()).toMatch(
+              /Daniel has 21 points, John has 22 points and Joe has 32/
+            )
+          })
+        })
+      })
+    })
+  })
+})
