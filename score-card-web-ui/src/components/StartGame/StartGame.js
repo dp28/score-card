8968b0dae1283e70cfc7da1e16f3956d040a4274
Daniel Patterson <dan24patt@gmail.com>
2018-07-01 16:53:04 +0100

    Enable starting a game with a name

--- score-card-web-ui/src/components/StartGame/StartGame.js
+++ score-card-web-ui/src/components/StartGame/StartGame.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import { startGame } from 'score-card-domain'
 
-export const StartGame = ({ dispatchStartGame }) => (
-  <Button variant="contained" color="primary" onClick={dispatchStartGame}>
-    Start game
-  </Button>
+const styles = {
+  textField: {
+    marginRight: 10
+  }
+}
+
+export const StartGame = ({ dispatchStartGame, classes }) => (
+  <form onSubmit={dispatchStartGame}>
+    <TextField
+      className={classes.textField}
+      id="gameName"
+      label="Game name"
+      margin="normal"
+    />
+    <Button type="submit" variant="contained" color="primary">
+      Start game
+    </Button>
+  </form>
 )
 
 function mapDispatchToProps(dispatch, { history }) {
   return {
-    dispatchStartGame: () => {
-      const event = startGame()
+    dispatchStartGame: submitEvent => {
+      submitEvent.preventDefault()
+      const gameName = submitEvent.target.gameName.value
+      submitEvent.target.value = ''
+      const event = startGame({ gameName })
       dispatch(event)
       history.push(`/games/${event.gameId}`)
     }
@@ -22,5 +41,5 @@ function mapDispatchToProps(dispatch, { history }) {
 }
 
 export const ConnectedStartGame = withRouter(
-  connect(null, mapDispatchToProps)(StartGame)
+  connect(null, mapDispatchToProps)(withStyles(styles)(StartGame))
 )
